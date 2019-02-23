6db1531121be8747ee7c9d46788bf7fe4f92bc08
lrolomeli <ie700093@iteso.mx>
2019-02-22 23:07:06 -0600

    primera version del modulo state_machine

--- t03/src/state_machine.sv
+++ t03/src/state_machine.sv
@@ -19,3 +19,76 @@
 * Tool versions: 
 *	Quartus Prime
 *********************************************************************************/
+
+module state_machine
+(
+	input clk,
+	input rst,
+	input start,
+	
+	output logic led_1hz,
+	output logic led_state
+);
+
+logic [1:0] state;
+logic [1:0] flag;
+logic [2:0] count_on;
+logic [2:0] count_off;
+
+always_ff@(posedge clk or negedge rst)
+begin
+
+	if(~rst)
+	begin
+		state <= '0;
+		count_on <= '0;
+		count_off <= '0;
+		led_1hz <= 1'b1;
+		led_state <= 1'b1;
+	end
+	
+	else
+	begin
+	
+		led_1hz <= ~led_1hz;
+		
+		case(state)
+			2'b00:
+			if(start)
+			begin
+				state <= 2'b01;
+			end
+			
+			2'b01:
+			if(count_on < 2'd6)
+			begin
+				led_state <= '0;
+				count_off <= '0;
+			end
+			else
+			begin
+				state <= 2'b10;
+			end
+			
+			2'b10:
+			if(count_off < 2'd4)
+			begin
+				led_state <= 1'b1;
+				count_on <= '0;
+			end
+			else
+			begin
+				flag <= flag + 2'b01;
+				state <= (flag == 2'b11) ? 2'b00 : 2'b01;
+			end
+			
+			default:
+			state <= 2'b00;
+
+		endcase
+		
+	end
+	
+end
+
+endmodule
