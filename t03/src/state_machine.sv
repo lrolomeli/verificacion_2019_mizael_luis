26d4d07594db2a5c790f8c104374f95d7569bb3b
lrolomeli <ie700093@iteso.mx>
2019-02-22 21:12:37 -0600

    carpeta de tarea 3 agregada

---
+++ t03/src/state_machine.sv
@@ -0,0 +1,21 @@
+/*********************************************************************************
+* Module Name:  
+*	state_machine.sv
+* Description:
+* Inputs:
+* Outputs:
+* Version:
+*	1.0
+* Company: 
+*	ITESO
+* Engineers: 
+*	Luis Roberto Lomeli Plascencia, Jorge Mizael Rodriguez Gutierrez
+* Create Date:  
+*	04/02/2019
+* Project Name: 
+*	t03
+* Target Devices: 
+*	FPGA ALTERA DE2-115
+* Tool versions: 
+*	Quartus Prime
+*********************************************************************************/
