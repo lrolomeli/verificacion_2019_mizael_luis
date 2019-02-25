afde6daaf579666339549a791ad575b5bbddd221
lrolomeli <ie700093@iteso.mx>
2019-02-24 21:10:10 -0600

    reparacion de ciertos errores en testbench como instanciacion de modulos, aun con errores, todo funciona al 100 por ciento cuando se forzan valores en la simulacion

--- t03/tb/tb_top_module.sv
+++ t03/tb/tb_top_module.sv
@@ -7,13 +7,23 @@ logic start;
 logic led_1hz;
 logic led_state;
 
+top_module top_module(
+
+	.clk(clk),
+	.rst(rst),
+	.start(start),
+	.led_1hz(led_1hz),
+	.led_state(led_state)
+
+);
+
 initial begin
         clk = 0;
         rst = 0;
 	start = 0;
     #2  rst = 1;
 	start = 1;
-    #120000000 
+    #1500000000
     $stop;
 end
 
